d05c6153ca3d89b6af6097e0e8606a935df412e3
omar <39051822+km@users.noreply.github.com>
2023-10-18 19:45:38 +0200

    Transaction class

---
+++ BitNav/Transaction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitNav
+{
+    public class Transaction
+    {
+        private object coin;
+        private int amount;
+        private string address;
+        private string senderAddress;
+        private string transactionHash;
+
+        public Transaction(object coin, int amount, string address, string senderAddress = null)
+        {
+            this.coin = coin;
+            this.amount = amount;
+            this.address = address;
+            this.senderAddress = senderAddress;
+            this.transactionHash = null;
+        }
+
+        //returns true if transaction is received
+        public bool checkTransaction()
+        {
+            return true;
+        }
+
+        //returns null if transaction is not received or the transaction hash if it is
+        public string getTransactionHash()
+        {
+            return this.transactionHash;
+        }
+
+        public string getSenderAddress()
+        {
+            return senderAddress;
+        }
+
+        public string getReceiverAddress()
+        {
+            return address;
+        }
+    }
+}
