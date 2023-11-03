39c2bc6370e1432c990962f4b3341b207193e8d4
omar <39051822+km@users.noreply.github.com>
2023-11-03 21:28:46 +0100

    TransactionManager Class Thread
    -Added thread that checks the transactions every 2 minutes to TransactionManager
    -Events will automatically be called anytime a transaction is received or confirmed

--- BitNav/TransactionManager.cs
+++ BitNav/TransactionManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NBitcoin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,20 +11,80 @@ namespace BitNav
     public class TransactionManager
     {
         private List<Transaction> transactions;
+        private List<Transaction> unconfirmedTransactions;
         private List<Transaction> confirmedTransactions;
-
+        
+        private Thread transactionChecker;
 
         public TransactionManager()
         {
-            
+            transactions = new List<Transaction>();
+            unconfirmedTransactions = new List<Transaction>();
+            confirmedTransactions = new List<Transaction>();
+            transactionChecker = transactionCheckerThread();
+            transactionChecker.Start();
         }
 
+
+        private Thread transactionCheckerThread()
+        {
+            Thread t = new Thread(() => checkTransactions());
+            return t;
+        }
+
+        private void checkTransactions()
+        {
+            while (true)
+            {
+                List<Transaction> transactionsToRemove = new List<Transaction>();
+
+                lock (unconfirmedTransactions)
+                {
+                    lock (confirmedTransactions)
+                    {
+                        foreach (Transaction transaction in unconfirmedTransactions)
+                        {
+                            if (transaction.checkTransactionConfirmed())
+                            {
+                                confirmedTransactions.Add(transaction);
+                                transactionsToRemove.Add(transaction);
+
+                            }
+                        }
+
+                        foreach (Transaction transaction in transactionsToRemove)
+                        {
+                            unconfirmedTransactions.Remove(transaction);
+                        }
+                    }
+                }
+
+                Thread.Sleep(120000);
+            }
+
+        }
         //create Transaction
         public Transaction CreateTransaction(object coin, int amount, string address)
         {
             Transaction transaction = new Transaction(coin, amount, address);
             transactions.Add(transaction);
             return transaction;
+        }
+
+        //remove transaction
+        public void RemoveTransaction(Transaction transaction)
+        {
+            transactions.Remove(transaction);
+
+            lock (unconfirmedTransactions)
+            {
+                unconfirmedTransactions.Remove(transaction);
+            }
+
+            lock (confirmedTransactions)
+            {
+                confirmedTransactions.Remove(transaction);
+            }
         }
     }
 }
